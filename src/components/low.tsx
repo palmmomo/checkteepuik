e135d2fca6bd9bb39d579943ada4a5dab420277d
kassana artwichai <156319538+gssana2341@users.noreply.github.com>
2025-05-02 19:20:39 +0700

    ล่าสุด

---
+++ src/components/low.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import styles from "../app/page.module.css";
+
+export default function Low() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-custom">
+      <h1 className="text-3xl font-bold mb-4">
+        โอนจองต้องเช็คเครดิตได้ ต้องได้บ้านจริง
+      </h1>
+    </div>
+  );
+}
+
